5cd9150b4610f3b67f566024c2ec9b22db56fde6
Natalia PN <npelegero@gmail.com>
2019-03-11 10:04:13 +0100

    Create PokemonCard component

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -3,27 +3,29 @@ import PropTypes from "prop-types";
 
 class PokemonCard extends Component {
     render() {
+        const { pokemonsArray } = this.props;
+        const pokemon = pokemonsArray.map(pok=> pok);
         return(
             <Fragment>
             <div className="Image__container">
-                <img className="Pokemon__picture" alt="pokemon" src={image}></img>
-                <p className="pokemon__order">id / {id}</p>
+                <img className="Pokemon__picture" alt="pokemon" src={pokemon.sprites.front_default}></img>
+                <p className="pokemon__order">id / {pokemon.id}</p>
             </div>
             <div className="Pokemon__basic-info">
-                <p className="Pokemon__name">{name}</p>
-                <p className="Pokemon__height">{height}</p>
-                <p className="Pokemon__weight">{weight}</p>
+                <p className="Pokemon__name">{pokemon.name}</p>
+                <p className="Pokemon__height">{pokemon.height}</p>
+                <p className="Pokemon__weight">{pokemon.weight}</p>
             </div>
             <div className="Pokemon__behaviour">
                 <ul className="Pokemon__types">
-                    {types.map((item, index)=> {
+                    {pokemon.types.map((item, index)=> {
                         return(
                             <li className="Ability" key={index}>{item.type.name} </li>
                         )
                         })}
                 </ul>
                 <ul className="Pokemon__abilities">
-                    {abilities.map((item, index)=> {
+                    {pokemon.abilities.map((item, index)=> {
                         return(
                             <li className="Ability" key={index}>{item.ability.name} </li>
                         )
--- src/components/PokemonsList.js
+++ src/components/PokemonsList.js
@@ -7,15 +7,14 @@ class PokemonsList extends Component {
     render() {
         const { filterPokemons } = this.props;
 
-        console.log(filterPokemons)
         return(
             <ul className="Pokemons__list">
                 {filterPokemons.map(pok=> {
-                return(
-                    <li className="Pokemon__element" key={pok.id}>
-                        <PokemonsDetail image={pok.sprites.front_default} id={pok.id} name= {pok.name} types={pok.types} />
-                    </li>
-                );
+                    return(
+                        <li className="Pokemon__element" key={pok.id}>
+                            <PokemonsDetail image={pok.sprites.front_default} id={pok.id} name= {pok.name} types={pok.types} />
+                        </li>
+                    );
                 })}
           </ul> 
         );
