b1cb9e2e0c3feecf4002f38a0a1f2ed1e52b7c9d
Natalia PN <npelegero@gmail.com>
2019-03-12 22:55:02 +0100

    Remove Link decoration

--- src/components/PokemonsList.js
+++ src/components/PokemonsList.js
@@ -12,7 +12,7 @@ class PokemonsList extends Component {
                 {filterPokemons.map(pok=> {
                     return(
                         <li className="Pokemon__element" key={pok.id}>
-                            <Link to={`/PokemonCard/${pok.id}`}>
+                            <Link to={`/PokemonCard/${pok.id}`} className="route__link">
                             <PokemonsDetail image={pok.sprites.front_default} id={pok.id} name= {pok.name} types={pok.types} />
                             </Link>
                         </li>
